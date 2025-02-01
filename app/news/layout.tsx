37941c142287ca933cbe09b4fc8e8ad217d343de
ibuki1204 <167719088+ibuki1204@users.noreply.github.com>
2025-02-01 23:29:19 +0900

    finish

--- app/news/layout.tsx
+++ app/news/layout.tsx
@@ -1,15 +1,21 @@
 import Hero from '@/app/_components/Hero';
 import Sheet from '@/app/_components/Sheet';
 
-type Props = {
-    children: React.ReactNode;
-  };
+export const metadata = {
+  title: 'ニュース',
+};
 
-  export default function NewsLayout({ children }: Props) {
-    return (
-      <>
-        <Hero title="News" sub="ニュース" />
-        <Sheet>{children}</Sheet>
-      </>
-    );
-  }
+type Props = {
+  children: React.ReactNode;
+};
+
+export const revalidate = 60;
+
+export default function NewsLayout({ children }: Props) {
+  return (
+    <>
+      <Hero title="News" sub="ニュース" />
+      <Sheet>{children}</Sheet>
+    </>
+  );
+}
